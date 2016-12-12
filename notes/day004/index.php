b9bf2131e16b1697b91fd460ed0ed4e44f687526
Uzzal Howlader <uzzalhowlader.ma@gmail.com>
2016-12-12 11:54:41 +0600

    namespace added in index.php

--- notes/day004/index.php
+++ notes/day004/index.php
@@ -1,6 +1,6 @@
 <?php
 
-
+namespace myname;
 
 echo __LINE__;
 //it show line no of script
@@ -14,3 +14,17 @@ echo __FILE__;
 // it show file absolute link
 //C:\xampp\htdocs\rayhan\seip-php\notes\day004\index.php
 
+echo __DIR__;
+
+// it show directory not present file name
+//C:\xampp\htdocs\rayhan\seip-php\notes\day004
+
+echo __FUNCTION__;
+
+function myfunc(){
+   echo "<br/>";
+    echo __FUNCTION__;
+    //show running function name
+}
+
+myfunc();
