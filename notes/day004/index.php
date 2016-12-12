5dd316227c8a00bf4694927b2d10eb97c2218608
Uzzal Howlader <uzzalhowlader.ma@gmail.com>
2016-12-12 11:48:35 +0600

    __File__ added in index.php

--- notes/day004/index.php
+++ notes/day004/index.php
@@ -4,3 +4,13 @@
 
 echo __LINE__;
 //it show line no of script
+
+echo __NAMESPACE__;
+
+//show namepsace name ;
+
+echo __FILE__;
+
+// it show file absolute link
+//C:\xampp\htdocs\rayhan\seip-php\notes\day004\index.php
+
