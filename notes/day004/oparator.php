f7aa9ca3b796f49320f43a699442c26d812c7433
Uzzal Howlader <uzzalhowlader.ma@gmail.com>
2016-12-12 12:15:31 +0600

    assignment operator completed

--- notes/day004/oparator.php
+++ notes/day004/oparator.php
@@ -2,8 +2,19 @@
 
 
 $x=10;
-$y=10;
+$y=3;
 
 $z=2;
 
-echo $x**$y;
+echo $x**$y."<br/>";
+// $x power of $y
+
+echo ' $x : '. $x." <br/>";
+$x = $y;
+echo 'now $x :';
+//cause $y assign in $x;
+echo $x."<br/>";
+// it (%) return devide result
+echo $x%$y."</br>";
+// it (/) return fraction
+echo $x/$y;
