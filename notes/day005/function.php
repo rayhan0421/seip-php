a9bc6190aee67419b72b40d6a9b958835e0619de
Rayhan Uddin <rayhan0421@gmail.com>
2016-12-14 11:38:38 +0600

    class 005 started

---
+++ notes/day005/function.php
@@ -0,0 +1,27 @@
+<?php 
+
+
+echo "function define";
+
+echo "<br/>";
+
+function getName()
+{
+	echo "Google";
+}
+
+getName();
+
+function getResult()
+{
+	$a=10;
+	$b=20;
+	
+	//echo $a+$b;
+	
+	return $a+$b;
+}
+echo "<br/>";
+//getResult();
+
+echo getResult();
