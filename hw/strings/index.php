a01ec5667a6f55e49a8e8c04457a0345a6f45b82
Rayhan Uddin <rayhan0421@gmail.com>
2016-12-14 21:23:10 +0600

    chop function added

--- hw/strings/index.php
+++ hw/strings/index.php
@@ -4,3 +4,18 @@
 
 echo '<center><h1>php chop()</h1></center>';
 
+$str = "Hello World!\n\n";
+echo $str;
+echo chop($str);
+
+/*
+ *
+ * The following characters are removed if the charlist parameter is empty:
+"\0" - NULL
+"\t" - tab
+"\n" - new line
+"\x0B" - vertical tab
+"\r" - carriage return
+" " - ordinary white space
+ */
+
