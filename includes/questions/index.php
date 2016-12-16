f5c2bc7cf2929c6e27d5a3d282ad1e2fc9fb6807
Rayhan Uddin <rayhan0421@gmail.com>
2016-12-16 09:17:07 +0600

    function array return example added in day005/hw

--- includes/questions/index.php
+++ includes/questions/index.php
@@ -1,6 +1,12 @@
 <?php
+echo "<input type='checkbox' checked>"."example checked"."<br/>";
+echo "<input type='checkbox' >"."license system"."<br/>";
+echo "<input type='checkbox' >"."money format english to bangla (invoice)"."<br/>";
 
-echo "license system"."<br/>";
-echo "money format english to bangla (invoice)"."<br/>";
+echo "<input type='checkbox' >"."money : number to word (invoice: 1001 (one thousand and one taka))"."<br/>";
 
-echo "money : number to word (invoice: 1001 (one thousand and one taka))."<br/>";
+
+echo "<input type='checkbox' >"."money : number to word (invoice: 1001 (one thousand and one taka))"."<br/>";
+
+
+
