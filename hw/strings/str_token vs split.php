31a71989927f8ad4fa89f9a29a544c31fe4be1c7
Rayhan Uddin <rayhan0421@gmail.com>
2016-12-16 13:00:15 +0600

    strtok and str_split added

---
+++ hw/strings/str_token vs split.php
@@ -0,0 +1,38 @@
+<?php
+
+$string = "This is\tan example\nstring";
+/* Use tab and newline as tokenizing characters as well  */
+$tok = strtok($string, " \n\t");
+
+while ($tok !== false) {
+    echo "Word=$tok<br />";
+    $tok = strtok(" \n\t");
+}
+
+
+echo ""."<br/>";
+
+
+
+$first_token  = strtok('/something', '/');
+$second_token = strtok('/');
+var_dump($first_token, $second_token);
+
+echo "<hr/>";
+echo "<pre>";
+print_r(str_split($string,3));
+echo "</pre>";
+
+echo "<br/>"."simple example";
+
+$str = "Hello Friend";
+
+$arr1 = str_split($str);
+$arr2 = str_split($str, 3);
+echo "<pre>";
+echo "without argument it take 1 char to array"."<br/>";
+print_r($arr1);
+
+echo "with argument it take 3 char to array and // space is a char "."<br/>";
+print_r($arr2);
+echo "</pre>";
