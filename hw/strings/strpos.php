44d80ba6ba0494f754fa1dcfda521b7224378e2f
Rayhan Uddin <rayhan0421@gmail.com>
2016-12-17 11:42:38 +0600

    strpos completed

---
+++ hw/strings/strpos.php
@@ -0,0 +1,34 @@
+<?php
+
+function strpos_all($haystack, $needle) {
+    $offset = 0;
+    $allpos = array();
+    while (($pos = strpos($haystack, $needle, $offset)) !== FALSE) {
+        $offset   = $pos + 1;
+        $allpos[] = $pos;
+    }
+    return $allpos;
+}
+echo "<pre>";
+print_r(strpos_all("aaa bbb aaa bbb aaa bbb", "aa"));
+echo "</pre>";
+
+echo "<br/>";
+
+
+$str_test = "Hello World! welcome to php";
+
+$count = 0;
+$find = "o";
+$positions = array();
+for($i = 0; $i<strlen($str_test); $i++)
+{
+    $pos = strpos($str_test, $find, $count);
+    if($pos == $count){
+        $positions[] = $pos;
+    }
+    $count++;
+}
+foreach ($positions as $value) {
+    echo '<br/>' .  $value . "<br />";
+}
