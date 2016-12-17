2ba146862bf807d3a7a8cd096fd77555b7df531c
Rayhan Uddin <rayhan0421@gmail.com>
2016-12-17 11:26:22 +0600

    before day06 string practice

--- notes/day006/addslashs.php
+++ notes/day006/addslashs.php
@@ -6,3 +6,7 @@ echo "<hr/>";
 
 echo addcslashes('hello sc word script sc', 'script');
 echo addcslashes('hello sc word script sc', 's');
+
+echo "<hr/>";
+$crc_string = crc32('Good Morning....!');
+printf('%u', $crc_string);
---
+++ notes/day006/create.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>form test</title>
+</head>
+<body>
+
+<form action="" method="">
+
+   <label>Text 1 : </label> <input type="text"  placeholder="text 1" />
+    <label>Text 2 : </label> <input type="text"  placeholder="text 1" />
+
+    <input type="submit" value="submit">
+
+</form>
+
+</body>
+</html>
