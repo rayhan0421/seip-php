fbbe89a0c2525161a9013f08be6de0e78376582c
Rayhan Uddin <rayhan0421@gmail.com>
2016-12-19 09:26:51 +0600

    strpos bangla using mb_strpos added

---
+++ notes/day007/personal/strpos.php
@@ -0,0 +1,11 @@
+<?php
+
+
+echo "strpos bangla";
+
+// bangla strpos
+// find position of string unicode
+
+echo "<br/> position is : ";
+
+echo mb_strpos("আসুস, ডেল, এইচপি, লেনোভো, ওয়ালটন, লাভা, অ্যাভিরা, আই-লাইফ, ইসেট, রাপ্পো","ই");
