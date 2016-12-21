07d80767cbe390b1f6369fa886dc130adbadcfa6
Rayhan Uddin <rayhan0421@gmail.com>
2016-12-21 09:11:31 +0600

    foreach added

--- notes/day007/array/foreach.php
+++ notes/day007/array/foreach.php
@@ -1,1 +1,17 @@
-// foreach
+<?php
+ for($i = 0;$i<10;$i++)
+ {
+     if($i%2== 0){
+         echo"Even number : ".$i."<br/>";
+     }
+ }
+ echo"<br/>";
+for($i = 0;$i<10;$i++)
+{
+    if($i%2== 1){
+        echo"Odd number : ".$i."<br/>";
+    }
+}
+echo"<br/>";
+for
+
