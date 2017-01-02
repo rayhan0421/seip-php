3b3fce028d0da8eea66a7aad676fec0f1e21025e
Rayhan Uddin <rayhan0421@gmail.com>
2017-01-02 13:01:53 +0600

    day013 added

---
+++ notes/day013/cw/oop_calc/calc.php
@@ -0,0 +1,35 @@
+<html>
+
+<head>
+    <title></title>
+</head>
+
+<body>
+
+<h1>Calculator Form </h1><hr/><br/>
+
+<form action="calc_action.php" method="get">
+
+  <label>Number 1 :</label>  <input type="text" name="number1" value="">
+  <label>Number 2 :</label>  <input type="text" name="number2" value="">
+    <input type="submit" name="type" value="Add">
+    <input type="submit" name="type" value="Sub">
+    <input type="submit" name="type" value="Mul">
+    <input type="submit" name="type" value="Div">
+
+
+</form>
+
+
+
+</body>
+</html>
+
+
+
+<?php
+
+
+
+
+
---
+++ notes/day013/cw/oop_calc/calc_class.php
@@ -0,0 +1,45 @@
+<?php
+
+Class MyCalc {
+
+     public $number1= null;
+
+    public $number2 = null;
+
+   public function __construct($num1,$num2)
+   {
+       $this->number1 = $num1;
+       $this->number2 = $num2;
+   }
+
+    public function add(){
+
+       try{
+
+       }catch (
+
+       )
+        return $this->number1+$this->number2;
+    }
+
+    public function sub(){
+
+
+
+        return $this->number1-$this->number2;
+    }
+
+    public function mul(){
+
+
+
+        return $this->number1*$this->number2;
+    }
+
+    public function div(){
+
+
+        return $this->number1/$this->number2;
+    }
+
+}
