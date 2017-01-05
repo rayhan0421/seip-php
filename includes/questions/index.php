bd5fb9361c360d69471b187ab9b31532013e4f7b
Rayhan Uddin <rayhan0421@gmail.com>
2017-01-05 19:24:04 +0600

    composer file autoload added

--- includes/questions/index.php
+++ includes/questions/index.php
@@ -9,13 +9,13 @@ echo "<input type='checkbox' >"."money : number to word (invoice: 1001 (one thou
 echo "<input type='checkbox' >"."Regular expression"."<br/>";
 echo "<input type='checkbox' >"."static variable vs local variable"."<br/>";
 
-echo "<input type='checkbox' >"."anonymous function  Clojure"."<br/>";
+echo "<input type='checkbox' >"."anonymous function  Closure"."<br/>";
 
 
 
 echo "<input type='checkbox' >"."what is domain model"."<br/>";
 
-echo "<input type='checkbox' >"."agile kanban vs scrum"."<br/>";
+echo "<input type='checkbox' >"."spl: advantage ,disadvantage, uniquness service"."<br/>";
 
 
 
