b8f638796a02214265e35695538ebd346e192861
Rayhan Uddin <rayhan0421@gmail.com>
2017-01-06 21:48:19 +0600

    composer project updated

--- hw/Composer_autoload/sample_project/src/admin/logger/logger.php
+++ hw/Composer_autoload/sample_project/src/admin/logger/logger.php
@@ -3,11 +3,17 @@ namespace Admin\logger;
 
 class logger{
 
+    public $name='Rayhan';
     public function __construct()
     {
         echo "<br/> i am logger file";
 
 
+    }
+
+    public function display(){
+
+        echo "display here <br/> ".$this->name;
     }
 }
 
---
+++ hw/Composer_autoload/sample_project/src/system/trans.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\system;
+
+Class trans{
+ public function __construct()
+ {
+     echo "trsn";
+ }
+}
