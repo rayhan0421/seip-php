712942e89fb7ddbc29e4aa72294282a9337d3385
Rayhan Uddin <rayhan0421@gmail.com>
2017-01-18 12:57:02 +0600

    day019 added

---
+++ notes/day019/Project01/views/bitm/seip/students/create.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+
+    <title>Student Information</title>
+</head>
+<body>
+
+<fieldset>
+
+    <legend>Add student information</legend>
+     <form action="store.php" method="post">
+
+         <input type="text" name="title" />
+         <input type="submit" value="submit" />
+
+     </form>
+</fieldset>
+
+</body>
+</html>
---
+++ notes/day019/Project01/views/bitm/seip/students/index.php
@@ -0,0 +1,57 @@
+<?php
+include ("../../../../vendor/autoload.php");
+use App\bitm\seip\students\students;
+$student = new students();
+
+
+?>
+<html>
+
+<head>
+    <title>
+        student list
+    </title>
+
+</head>
+<body>
+<h1>student list</h1>
+<hr/>
+
+<?php
+
+$student = $student->index();
+$sel = 0;
+
+
+?>
+
+<table width="50%">
+    <tr style="background-color: grey">
+        <td>ID</td>
+        <td>Title</td>
+        <td>Action</td>
+
+    </tr>
+
+    <?php   foreach ($student as $value){
+  $sel++;
+
+     ?>
+
+
+
+    <tr>
+        <td><?php echo $sel; ?></td>
+         <td><?php echo $value['title']; ?></td>
+        <td><a href="show.php?id=<?php echo $value['id']; ?>">views details</a> / <a href="edit.php?id=<?php echo $value['id']; ?>">edit</a> </td>
+    </tr>
+
+    <?php } ?>
+
+
+
+
+</table>
+
+</body>
+</html>
